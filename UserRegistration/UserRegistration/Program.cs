2ec9d68ff9079b1ff9418e213f86f94826c220e5
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-09 13:14:47 +0530

    [shabana] added UC1-FirstName

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Validating using Regular Expression!");
+            RegexSample regexSample = new RegexSample();
+            regexSample.Validating();
+        }
+    }
+}
