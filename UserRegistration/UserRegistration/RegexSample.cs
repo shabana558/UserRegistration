e0cc4069418177387be480b8e4b6174d2a0dca40
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-09 14:04:40 +0530

    [shabana] added UC6-1UpperCase

--- UserRegistration/UserRegistration/RegexSample.cs
+++ UserRegistration/UserRegistration/RegexSample.cs
@@ -13,20 +13,93 @@ namespace UserRegistration
 
         string pattern = "^[A-Za-z]{2,}$";
 
+
         public void Validating()
         {
+            
             Regex regex = new Regex(pattern);
-
+            Console.WriteLine("Validating the first name");
+            ValidatingFirstName();
+            Console.WriteLine("Validating The Last Name");
+            ValidatingLastName();            
+            Console.WriteLine("Validating Email Address");
+            ValidatingEmailId();         
+            Console.WriteLine("Validating Phone Number");
+            ValidatingPhoneNum();
             Console.WriteLine("Validating password");
-            ValidatingPassWord();
+            ValidatingPassWord(); 
 
         }
+        public void ValidatingFirstName()
+        {
+            Regex Regex = new Regex(pattern);
+            Console.WriteLine("Enter the first name:");
+            string input = Console.ReadLine();
+            bool res = Regex.IsMatch(input);
+            if (res)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+        public void ValidatingLastName()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Enter the last name : ");
+            string input1 = Console.ReadLine();
+            bool res1 = regex.IsMatch(input1);
+            if (res1)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+        public void ValidatingEmailId()
+        {
+            string emailPattern = (@"^[a-zA-Z0-9]+([\.\+\-][a-zA-Z0-9]+)?@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,}(\.[a-zA-Z]+)?)$");
+            Regex regex = new Regex(emailPattern);
+            Console.WriteLine("Enter email Id");
+            string emails = Console.ReadLine();
+            bool res = regex.IsMatch(emails);
+            if (res)
+            {
+                Console.WriteLine("valid email address");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a Valid Email!");
+            }
+        }
+        public void ValidatingPhoneNum()
+        {
+            string phoneNumPattern = @"^[0-9]+[\s]+[0-9]{10}$";
+            Regex regex = new Regex(phoneNumPattern);
+            Console.WriteLine("Enter valid Phone Number");
+            string phoneNumber = Console.ReadLine();
+            bool res = regex.IsMatch(phoneNumber);
+            if (res)
+            {
+                Console.WriteLine("valid phone number");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Phone Number");
+            }
 
+
+        }
         public void ValidatingPassWord()
         {
-            string passwordPattern = @"[a-z,A-Z,0-9]{8,}$";
+            //string passwordPattern = @"[a-z,A-Z,0-9]{8,}$";
+            string passwordPattern = @"^(?=.*[A-Z]).{8,}$";
             Regex regex = new Regex(passwordPattern);
-            Console.WriteLine("Enter password minimum 8 characters");
+            Console.WriteLine("Enter password minimum 8 characters with one upper case");
             string password = Console.ReadLine();
             bool res = regex.IsMatch(password);
             if (res)
@@ -37,7 +110,6 @@ namespace UserRegistration
             {
                 Console.WriteLine("invalid password");
             }
-
         }
     }
 }
